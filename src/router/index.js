21dd8f11183c9ee56874dfdcce2dcd7261e7864f
BA4413 <2073600373@qq.com>
2024-05-13 00:10:08 +0800

    2024/5/13

--- src/router/index.js
+++ src/router/index.js
@@ -3,7 +3,7 @@ import { createRouter, createWebHistory } from "vue-router"
 const routes = [
     {
         path:"/",
-        // alias: "/index",
+        alias: "/index",
         component: () => import("../page/index/index.vue")
     },
     {
